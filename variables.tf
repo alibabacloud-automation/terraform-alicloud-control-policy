143b4be1dd153c182d9325110e816bf997fff306
shanye <shanye.xzq@alibaba-inc.com>
2024-09-02 10:13:59 +0800

    module: support data source alicloud_resource_manager_resource_directories; example/complete: improve the examples

--- variables.tf
+++ variables.tf
@@ -1,31 +1,32 @@
-variable resource_manager_folder_ids {
+variable "resource_manager_folder_ids" {
   description = "Bound resource folder ID group"
+  type        = list(string)
   default     = []
 }
 
-variable control_policy_name {
+variable "control_policy_name" {
   description = "Policy name"
   type        = string
   default     = "policy_name"
 }
 
-variable description {
+variable "description" {
   description = "Policy description"
   type        = string
   default     = ""
 }
 
-variable effect_scope {
+variable "effect_scope" {
   description = "Policy scope"
   type        = string
   default     = "RAM"
 }
 
-variable policy_document {
+variable "policy_document" {
   description = "Policy configuration"
-  default     = {
-    "Version":"1","Statement": [{
-      "Effect": "Deny","Action": ["ram:UpdateRole","ram:DeleteRole","ram:AttachPolicyToRole","ram:DetachPolicyFromRole"],"Resource":"acs:ram:*:*:role/ResourceDirectoryAccountAccessRole"
+  default = {
+    "Version" : "1", "Statement" : [{
+      "Effect" : "Deny", "Action" : ["ram:UpdateRole", "ram:DeleteRole", "ram:AttachPolicyToRole", "ram:DetachPolicyFromRole"], "Resource" : "acs:ram:*:*:role/ResourceDirectoryAccountAccessRole"
     }]
   }
 }
